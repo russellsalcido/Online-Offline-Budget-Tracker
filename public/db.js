0f11989bc51662c45115e20ec4b972b9cf4ba8f5
Russell Salcido <russell.salcido@gmail.com>
2020-07-01 18:18:24 -0700

    built indexedDB in db.js

--- public/db.js
+++ public/db.js
@@ -8,12 +8,8 @@ request.onupgradeneeded = function(event) {
 
   // Create an objectStore
   // including a auto-incrementing key
-  let objectStore = db.createObjectStore('transaction_os', { keyPath: 'id', autoIncrement:true });
-
   // Define what data items the objectStore will contain
-  objectStore.createIndex('transaction', 'transaction', { unique: false });
-
-  console.log('Database setup complete');
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -29,15 +25,21 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-  // create a transaction on the pending db with readwrite access
   // access your pending object store
+  const transaction = db.transaction(["pending"], "readwrite");
+  // create a transaction on the pending db with readwrite access
+  const store = transaction.objectStore("pending");
   // add record to your store with add method.
+  store.add(record);
 }
 
 function checkDatabase() {
   // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
   // access your pending object store
+  const store = transaction.objectStore("pending");
   // get all records from store and set to a variable
+  const getAll = store.getAll();
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
@@ -49,19 +51,15 @@ function checkDatabase() {
           "Content-Type": "application/json"
         }
       })
-      .then(response => response.json())
-      .then((data => {
+      .then((response) => response.json())
+      .then(() => {
           // if successful, open a transaction on your pending db
-        if (data.errors) {
-          errorEl.textContent = "Missing Information";
-        }
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
           // clear all items in your store
-          else {
-            nameEl.value = "";
-            amountEl.value = "";
-        }
-      }))
-    };
+          store.clear();
+        });
+    }
   };
 }
 
