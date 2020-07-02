5b179020d60acb0403e3f4ac4039c0a9e42371d4
Russell Salcido <russell.salcido@gmail.com>
2020-07-01 19:28:42 -0700

    updated for heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 const FILES_TO_CACHE = [
   '/index.html',
-  '/manifest.webmanifest',
   '/index.js',
+  '/manifest.webmanifest',
   '/db.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
+  '/styles.css'
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -44,9 +45,9 @@ self.addEventListener("activate", function(evt) {
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+      caches.open(DATA_CACHE_NAME).then((cache) => {
         return fetch(evt.request)
-          .then(response => {
+          .then((response) => {
             // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
@@ -54,18 +55,19 @@ self.addEventListener("fetch", function(evt) {
 
             return response;
           })
-          .catch(err => {
+          .catch((err) => {
             // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
+      })
+      .catch((err) => console.log(err))
     );
 
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.match(evt.request).then(response => {
         return response || fetch(evt.request);
       });
